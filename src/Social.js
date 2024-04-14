b610d5a41a1e3498cef899a58a3e6d8104a50b1f
di-feb <sdi1900045@di.uoa.gr>
2024-04-14 17:06:56 +0300

    name attribute

--- src/Social.js
+++ src/Social.js
@@ -6,13 +6,13 @@ import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 function Social({theme}) {
     return (
         <div className={`social_${theme}`}>
-            <a href="http://www.linkedin.com/in/stathis-demenagas" target="_blank" rel="noreferrer">
+            <a href="http://www.linkedin.com/in/stathis-demenagas" target="_blank" rel="noreferrer" name='LinkedIn'>
                 <LinkedInIcon className={`social_icon_${theme}`} sx={{ border: '50px'}} />
             </a>
-            <a href="https://github.com/di-feb" target="_blank" rel="noreferrer">
+            <a href="https://github.com/di-feb" target="_blank" rel="noreferrer" name='Github'>
                 <GitHubIcon className={`social_icon_${theme}`} />
             </a>
-            <a href="mailto:efstathiosdemenagas@gmail.com" target="_blank" rel="noreferrer">
+            <a href="mailto:efstathiosdemenagas@gmail.com" target="_blank" rel="noreferrer" name='Email'>
                 <AlternateEmailIcon className={`social_icon_${theme}`} />
             </a>
             <div className={`vertical_line_${theme}`}></div>
