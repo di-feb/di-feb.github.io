d2248c5bc14c713cff1eb8be9a8cd338ba777a57
di-feb <sdi1900045@di.uoa.gr>
2024-04-14 17:11:55 +0300

    aria-lebel attribute

--- src/Social.js
+++ src/Social.js
@@ -6,13 +6,13 @@ import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
 function Social({theme}) {
     return (
         <div className={`social_${theme}`}>
-            <a href="http://www.linkedin.com/in/stathis-demenagas" target="_blank" rel="noreferrer" name='LinkedIn'>
+            <a href="http://www.linkedin.com/in/stathis-demenagas" target="_blank" rel="noreferrer" aria-label='LinkedIn'>
                 <LinkedInIcon className={`social_icon_${theme}`} sx={{ border: '50px'}} />
             </a>
-            <a href="https://github.com/di-feb" target="_blank" rel="noreferrer" name='Github'>
+            <a href="https://github.com/di-feb" target="_blank" rel="noreferrer" aria-label='Github'>
                 <GitHubIcon className={`social_icon_${theme}`} />
             </a>
-            <a href="mailto:efstathiosdemenagas@gmail.com" target="_blank" rel="noreferrer" name='Email'>
+            <a href="mailto:efstathiosdemenagas@gmail.com" target="_blank" rel="noreferrer" aria-label='Email'>
                 <AlternateEmailIcon className={`social_icon_${theme}`} />
             </a>
             <div className={`vertical_line_${theme}`}></div>
